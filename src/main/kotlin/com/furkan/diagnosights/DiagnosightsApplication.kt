a98f6534c2aa95f687bfae20a6be8c04e3290345
afurkanerdem <abdullahfurkanerdem@gmail.com>
2024-07-22 23:09:14 +0300

    initial commit

---
+++ src/main/kotlin/com/furkan/diagnosights/DiagnosightsApplication.kt
@@ -0,0 +1,11 @@
+package com.furkan.diagnosights
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiagnosightsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiagnosightsApplication>(*args)
+}
---
+++ src/test/kotlin/com/furkan/diagnosights/DiagnosightsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.furkan.diagnosights
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiagnosightsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
