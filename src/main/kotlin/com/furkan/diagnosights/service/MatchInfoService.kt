0cd17c2af7a3fbf0f628e1d37d0b46b2c54c2040
afurkanerdem <abdullahfurkanerdem@gmail.com>
2024-12-08 17:00:11 +0300

    add cache to hold cache match.

--- src/main/kotlin/com/furkan/diagnosights/service/MatchInfoService.kt
+++ src/main/kotlin/com/furkan/diagnosights/service/MatchInfoService.kt
@@ -3,6 +3,8 @@ package com.furkan.diagnosights.service
 import com.furkan.diagnosights.model.LabRecord
 import com.furkan.diagnosights.model.MatchInfo
 import com.furkan.diagnosights.repository.MatchInfoRepository
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -12,8 +14,21 @@ class MatchInfoService {
     @Autowired
     private lateinit var matchInfoRepository: MatchInfoRepository
 
+    private val cache: Cache<String, MatchInfo> = Caffeine.newBuilder()
+        .maximumSize(1000)
+        .build()
+
     suspend fun updateMatchInfo(labRecords: List<LabRecord>) {
-        val derivedMatches = labRecords.map {  deriveMatchInfo(it) }
+
+        val derivedMatches = labRecords.map { deriveMatchInfo(it) }
+
+        derivedMatches.forEach {
+            it?.let {
+                val matchKey = generateCacheKeyOfMatchInfo(it)
+                cache.invalidate(matchKey)
+            }
+        }
+
         matchInfoRepository.upsertMatchInfo(derivedMatches.filterNotNull())
     }
 
@@ -21,15 +36,24 @@ class MatchInfoService {
         matchInfoRepository.deleteRecord(barcodeId)
     }
 
-    suspend fun getMatchInfo(labRecord: LabRecord) : MatchInfo? {
+    suspend fun getMatchInfo(labRecord: LabRecord): MatchInfo? {
 
         val matchInfo = deriveMatchInfo(labRecord)
-        return matchInfo?.let {  matchInfoRepository.getMatchInfo(matchInfo)}
+        return matchInfo?.let { derivedMatch ->
+
+            val matchKey = generateCacheKeyOfMatchInfo(derivedMatch)
+            val cachedMatchedInfo: MatchInfo? = cache.getIfPresent(matchKey)
+
+            if (cachedMatchedInfo != null) return cachedMatchedInfo
+
+            return matchInfoRepository.getMatchInfo(matchInfo)
+                .also { retrievedMatch -> retrievedMatch?.let { cache.put(matchKey, it) } }
+        }
     }
 
     private fun deriveMatchInfo(labRecord: LabRecord): MatchInfo? {
 
-        if (labRecord.resistantAntibiotics.isEmpty() || labRecord.susceptibleAntibiotics.isEmpty() || labRecord.susceptibleAtHighDoseAntibiotics.isEmpty()) return null
+        if (labRecord.resistantAntibiotics.isEmpty() && labRecord.susceptibleAntibiotics.isEmpty() && labRecord.susceptibleAtHighDoseAntibiotics.isEmpty()) return null
 
         val resistantAntibioticsHash = hashArrayIrrespectiveOrder(labRecord.resistantAntibiotics)
         val susceptibleAntibioticsHash = hashArrayIrrespectiveOrder(labRecord.susceptibleAntibiotics)
@@ -45,7 +69,14 @@ class MatchInfoService {
     }
 
 
-
-    private fun hashArrayIrrespectiveOrder(values: Array<String>) =
+    fun hashArrayIrrespectiveOrder(values: Array<String>) =
         hashSetOf(*values).hashCode().toString()
+
+
+     fun generateCacheKeyOfMatchInfo(matchInfo: MatchInfo): String {
+        return "${matchInfo.resistantAntibioticsHashValue}_${matchInfo.susceptibleAntibioticsHashValue}_${matchInfo.susceptibleAtHighDoseAntibioticsHashValue}"
+    }
+
 }
+
+
